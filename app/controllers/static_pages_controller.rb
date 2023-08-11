cb69902ef7676e9c65cdf95b484ec6f95a2e24f4
Daniel Neel <dneel10@gmail.com>
2023-08-10 22:04:17 -0400

    add random photos if no user id, users photos if id is provided

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index
+    begin
+      flickr = Flickr.new
+      
+      unless params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      else
+        @photos = flickr.photos.getRecent
+      end
+    end
+  end
+end
